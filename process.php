1067de020af74e09697bf9e932f787e44da42653
victor mejia <victorandymejiacastellanos@gmail.com>
2025-09-25 19:51:22 -0600

    fin practica

--- process.php
+++ process.php
@@ -2,7 +2,27 @@
 require("classes/estudiante.class.php");
 $Estudiante = new Estudiante();
 
-$resultado = $Estudiante->obtenerEstudiantes();
+$resultado = [];
+
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+    $tipo_peticion = ((isset($_GET["t"])) ? ($_GET["t"]) : null );
+    
+    if($tipo_peticion=="selectAll"){
+        $resultado = $Estudiante->obtenerEstudiantes();
+    }else{
+        $resultado = [];
+    }
+
+}elseif($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(isset($_POST["fnac"]) and isset($_POST["idg"])){
+        $resultado = $Estudiante->nuevoEstudiante($_POST["fnac"],$_POST["idg"]);
+    }else{
+        header('HTTP/1.1 400 Bad Request');
+        $resultado = array("mensaje" => "parametros no enviados" );
+    }
+}
+
+
 
 
 header("Content-Type: Application/json");
